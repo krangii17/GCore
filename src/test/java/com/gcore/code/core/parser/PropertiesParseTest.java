033c081d9754c58900b6c6d58725267ddad6bfcb
timyr <timyrle45@gmail.com>
2019-07-12 12:59:54 +0300

    has been added some new tests

--- src/test/java/com/gcore/code/core/parser/PropertiesParseTest.java
+++ src/test/java/com/gcore/code/core/parser/PropertiesParseTest.java
@@ -1,20 +1,56 @@
 package com.gcore.code.core.parser;
 
+import com.gcore.code.core.exception.PropertiesNotFoundException;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import static org.junit.Assert.*;
 
 public class PropertiesParseTest {
+    private PropertiesParse parse;
+    private PropertiesParse parseWithProperties;
+    private PropertiesParse falseProperties;
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
 
     @Before
     public void setUp() throws Exception {
+        parse = new PropertiesParse();
+        parseWithProperties = new PropertiesParse("src/test/resources/gcore.properties");
+        falseProperties = new PropertiesParse("this is false prop");
     }
 
     @After
     public void tearDown() throws Exception {
+        parse = null;
+        parseWithProperties = null;
+        falseProperties = null;
     }
 
     @Test
-    public void getFolder() {
+    public void getFolderWithoutArguments() {
+        //WHEN
+        String folder = parse.getFolder();
+        //THEN
+        assertEquals("com/gcore/code/test",folder);
+    }
+
+    @Test
+    public void getFolderWithArguments() {
+        //WHEN
+        String folder = parseWithProperties.getFolder();
+        //THEN
+        assertEquals("com/gcore/code/withargs",folder);
+    }
+
+    @Test()
+    public void cantFindProperties(){
+        thrown.expect(PropertiesNotFoundException.class);
+        thrown.expectMessage("Error to find properties file");
+        falseProperties.getFolder();
     }
 }
